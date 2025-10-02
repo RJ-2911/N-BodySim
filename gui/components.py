10a81a2e73812720a295268a76cbd720fd5b8fa0
Prem791 <155795044+Prem791@users.noreply.github.com>
2025-10-02 16:01:47 +0530

    Add files via upload

--- gui/components.py
+++ gui/components.py
@@ -8,6 +8,7 @@ class Color:
     text = (255,255,255)
     primary = (0,0,0)
     primaryV = (34,34,34)
+    primaryVL = (44,44,44)
     secondary = (29, 205, 159)
     secondaryV = (22, 153, 118)
 
@@ -18,18 +19,20 @@ class String:
     small_font_style = pygame.font.SysFont('Helvetica', font_size-10)
 
     info = font_style.render("Information Panel", True, Color.text)
-    add = small_font_style.render("Add", True, Color.text, Color.primaryV )
+    add = small_font_style.render("Add", True, Color.text, Color.primaryV)
+    addL = small_font_style.render("Add", True, Color.text, Color.primaryVL)
     remove = small_font_style.render("Remove", True, Color.text, Color.primaryV)
     change = small_font_style.render("Change", True, Color.text, Color.primaryV)
+    
 
 class Image:
     play = pygame.image.load("resources/play.png")
     pause = pygame.image.load("resources/pause.png")
-    plus5x = pygame.image.load("resources/+5x.png")
-    minus5x = pygame.image.load("resources/-5x.png")
+    fast = pygame.image.load("resources/fast.png")
+    reset = pygame.image.load("resources/reset.png")
 
     # for lighter version when hovered
     playL = pygame.image.load("resources/playL.png")
     pauseL = pygame.image.load("resources/pauseL.png")
-    plus5xL = pygame.image.load("resources/+5xL.png")
-    minus5xL = pygame.image.load("resources/-5xL.png")
+    fastL = pygame.image.load("resources/fastL.png")
+    resetL = pygame.image.load("resources/resetL.png")
