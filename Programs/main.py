8a93652f6471484aa39ee7cd843f29b3cdda0464
Rishit Joshi <joshirishit2005@gmail.com>
2025-10-05 11:21:46 +0530

    refreshin

--- Programs/main.py
+++
@@ -1,9 +0,0 @@
-import simWindow as sw
-
-class Main():
-    def __init__(self):
-        self.simWin = sw.SimWindow()
-        self.simWin.main_loop()
-        
-if __name__ == "__main__":
-    Main()
